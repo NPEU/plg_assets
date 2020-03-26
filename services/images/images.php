36a2c9f1ba4a6e8ae6ef22ccbd93bea185b32c02
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2020-03-26 17:51:01 +0000

    WiP

--- services/images/images.php
+++ services/images/images.php
@@ -3,7 +3,7 @@ require_once('ImageService.php');
 
 $path       = str_replace('?' . $_SERVER['QUERY_STRING'], '', $_SERVER['REQUEST_URI']);
 $pathinfo   = pathinfo($path);
-$cache_root = str_replace('/assets/images', '/cache/assets/images', $pathinfo['dirname']);
+$cache_root = str_replace('/assets/', '/cache/assets/', $pathinfo['dirname']);
 
 $img_service = new ImageService;
 $img_service->cache_root = $cache_root;
