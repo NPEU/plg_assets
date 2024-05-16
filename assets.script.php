a2d469f353d84132e1a2b9c46058f1f5c80b8f17
Andy Kirk <andy@andykirk.net>
2024-05-16 17:33:13 +0100

    J4

--- assets.script.php
+++ assets.script.php
@@ -1,4 +1,11 @@
 <?php
+namespace NPEU\Plugin\System\Assets;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Filesystem\File;
+use Joomla\CMS\Filesystem\Folder;
+
 class plgSystemAssetsInstallerScript
 {
     protected $plugin_dir = JPATH_ROOT . '/plugins/system/assets';
@@ -7,40 +14,40 @@ class plgSystemAssetsInstallerScript
     // (However, we should delete the htaccess file this plugin added).
     protected $assets_dir = '/assets';
 
-	/**
-	 * Called after any type of action
-	 *
-	 * @param   string  $route  Which action is happening (install|uninstall|discover_install)
-	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
-	 *
-	 * @return  boolean  True on success
-	 */
-	public function postflight($route, JAdapterInstance $adapter)
-	{
-		if (!($route == 'install' || $route == 'update')) {
-			return;
-		}
+    /**
+     * Called after any type of action
+     *
+     * @param   string  $route  Which action is happening (install|uninstall|discover_install)
+     * @param   JAdapterInstance  $adapter  The object responsible for running this script
+     *
+     * @return  boolean  True on success
+     */
+    public function postflight($route, JAdapterInstance $adapter)
+    {
+        if (!($route == 'install' || $route == 'update')) {
+            return;
+        }
 
         // Move latest version of htaccess file:
-        if (!JFolder::exists(JPATH_ROOT . $this->assets_dir)) {
-            JFolder::create(JPATH_ROOT . $this->assets_dir);
+        if (!Folder::exists(JPATH_ROOT . $this->assets_dir)) {
+            Folder::create(JPATH_ROOT . $this->assets_dir);
         }
-        JFile::move(
+        File::move(
             $this->plugin_dir . '/assets.htaccess.txt',
             JPATH_ROOT . $this->assets_dir . '/.htaccess'
         );
 
         return true;
-	}
+    }
 
-	/**
-	 * Called on uninstallation
-	 *
-	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
-	 */
-	public function uninstall(JAdapterInstance $adapter)
-	{
-        JFile::delete(JPATH_ROOT . $this->assets_dir . '/.htaccess');
-	}
+    /**
+     * Called on uninstallation
+     *
+     * @param   JAdapterInstance  $adapter  The object responsible for running this script
+     */
+    public function uninstall(JAdapterInstance $adapter)
+    {
+        File::delete(JPATH_ROOT . $this->assets_dir . '/.htaccess');
+    }
 }
 ?>
---
+++ js/assets.js
@@ -0,0 +1,111 @@
+/*
+
+*/
+
+(function() {
+
+    var tidy_media_views = function () {
+        //console.log('tidy_media_views');
+
+        const com_media = document.getElementById('com-media');
+
+        if (com_media) {
+            // Watch this for changes and then fix all visible filenames:
+            const config = { attributes: false, childList: true, subtree: true };
+
+            const callback = (mutationList, observer) => {
+                //console.log("com-media has changed");
+                let params = new URLSearchParams(document.location.search);
+                let path = params.get("path").replace('local-assets:', '/assets');
+
+                // I'm not sure if it's best to operate on each mutation to do the fixed, or to do
+                // it on the altered container as a whole?
+                // 2nd option would run the changes per-file, would this be very slow?
+
+                var elements_1 = com_media.querySelectorAll('.media-browser-item > :not(.media-browser-item-directory)');
+                Array.prototype.forEach.call(elements_1, function (el, i) {
+                    // Look to see if the image is set or not:
+                    var preview_container = el.querySelector('.media-browser-item-preview >.file-background');
+                    //return;
+                    if (preview_container) {
+                        preview_container.classList.remove('file-background');
+                        preview_container.classList.add('image-background');
+                        var img = preview_container.querySelector('img');
+                        if (!img) {
+                            let info_container = el.querySelector('.media-browser-item-info');
+                            let file = path + '/' + info_container.textContent.trim() + '.png';
+
+                            var width = '100';
+                            var height = '100';
+
+                            let img = `<img
+                                class="image-cropped"
+                                src="${file}"
+                                alt="${el.title}"
+                                loading="lazy"
+                                width="${width}"
+                                height="${height}"
+                            >`;
+                            preview_container.innerHTML = img;
+
+                            /*
+                            // Get image info:
+                            // UPDATE - note this was done BEFORE innerHTML but prefomed very badly
+                            // when there are a lot of files.The code used id below. It should be
+                            // possible to just do the innerHMTML then go back through and update
+                            // width/heigh, but there doesn't seem much point as those attributes
+                            // are not acually doing anything for the display (that I can see)
+                            var json_url = window.location.origin + file + '.preview.json';
+
+                            fetch(json_url)
+                                .then((response) => {
+                                    if (!response.ok) {
+                                        throw new Error(`HTTP error: ${response.status}`);
+                                    }
+                                    return response.text();
+                                })
+                                .then((text) => {
+                                    img_info = JSON.parse(text);
+                                    width = img_info.image_width;
+                                    height = img_info.image_height;
+
+                                    let img = `<img
+                                        class="image-cropped"
+                                        src="${file}"
+                                        alt="${el.title}"
+                                        loading="lazy"
+                                        width="${width}"
+                                        height="${height}"
+                                    >`;
+                                    container.innerHTML = img;
+                                })
+                                .catch((error) => {
+                                    console.warn(error);
+                                });
+                            */
+                        }
+                    }
+                })
+
+            };
+
+            const observer = new MutationObserver(callback);
+            observer.observe(com_media, config);
+        }
+    };
+
+    const querySelectorFrom = (selector, elements) => {
+        const elementsArr = [...elements];
+        return [...document.querySelectorAll(selector)].filter(elm => elementsArr.includes(elm));
+    }
+
+    const ready = function(fn) {
+        if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+            fn();
+        } else {
+            document.addEventListener('DOMContentLoaded', fn);
+        }
+    }
+
+    ready(tidy_media_views);
+})();
