1ac0f8d7c674c6ab4e418655177c7d90cd608c9f
Andy Kirk <andy@andykirk.net>
2025-11-13 09:32:47 +0000

    Fix issue with unlock files in gallery mode.

--- js/assets.js
+++ js/assets.js
@@ -25,84 +25,89 @@
                 var elements_1 = com_media.querySelectorAll('.media-browser-item > :not(.media-browser-item-directory)');
                 //console.log(path);
                 Array.prototype.forEach.call(elements_1, function (el, i) {
-                    // Check for type of item:
-                    var item_container = el.querySelector('.media-browser-item-preview');
-                    var filename = item_container.getAttribute('title');
-                    var extension = filename.split('.').pop();
-                    //console.log(extension);
-                    if (!extension.match(/(jpg|png)/)) {
-                        el.classList.add('is_file');
-                        el.dataset.filetype = extension;
-                    }
 
-                    // Look to see if the image is set or not:
-                    var preview_container = item_container.querySelector('.image-background');
-                    //
-                    //return;
-                    if (preview_container) {
+                    var info_container = el.querySelector('.media-browser-item-info');
+                    console.log(info_container);
+                    if (info_container.textContent.trim().endsWith('.unlock')) {
+                        el.parentNode.setAttribute("hidden", true);
 
-                        // Special handling for ZIP files:
-                        if (extension == 'zip') {
+                    } else {
 
+                        // Check for type of item:
+                        var item_container = el.querySelector('.media-browser-item-preview');
+                        var filename = item_container.getAttribute('title');
+                        var extension = filename.split('.').pop();
+                        //console.log(extension);
+                        if (!extension.match(/(jpg|png)/)) {
+                            el.classList.add('is_file');
+                            el.dataset.filetype = extension;
+                        }
 
+                        // Look to see if the image is set or not:
+                        var preview_container = item_container.querySelector('.image-background');
+                        //
+                        //return;
+                        if (preview_container) {
 
-                            item_container.innerHTML = '<div class="file-background"><div class="folder-icon"><span class="fa fa-file-archive"></span></div></div>';
+                            // Special handling for ZIP files:
+                            if (extension == 'zip') {
+                                item_container.innerHTML = '<div class="file-background"><div class="folder-icon"><span class="fa fa-file-archive"></span></div></div>';
+                            } else {
+                                //preview_container.classList.add('image-background');
+                                var img = preview_container.querySelector('img');
+                                if (!img) {
+                                    let info_container = el.querySelector('.media-browser-item-info');
+                                    let file = path + '/' + info_container.textContent.trim() + '.png';
+                                    //console.log(info_container.textContent);
+                                    var width = '100';
+                                    var height = '100';
 
-                        } else {
-                            //preview_container.classList.add('image-background');
-                            var img = preview_container.querySelector('img');
-                            if (!img) {
-                                let info_container = el.querySelector('.media-browser-item-info');
-                                let file = path + '/' + info_container.textContent.trim() + '.png';
-                                //console.log(info_container.textContent);
-                                var width = '100';
-                                var height = '100';
+                                    let img = `<img
+                                        class="image-cropped"
+                                        src="${file}"
+                                        alt="${el.title}"
+                                        loading="lazy"
+                                        width="${width}"
+                                        height="${height}"
+                                    >`;
+                                    preview_container.innerHTML = img;
 
-                                let img = `<img
-                                    class="image-cropped"
-                                    src="${file}"
-                                    alt="${el.title}"
-                                    loading="lazy"
-                                    width="${width}"
-                                    height="${height}"
-                                >`;
-                                preview_container.innerHTML = img;
+                                    /*
+                                    // Get image info:
+                                    // UPDATE - note this was done BEFORE innerHTML but prefomed very badly
+                                    // when there are a lot of files.The code used id below. It should be
+                                    // possible to just do the innerHMTML then go back through and update
+                                    // width/heigh, but there doesn't seem much point as those attributes
+                                    // are not acually doing anything for the display (that I can see)
+                                    var json_url = window.location.origin + file + '.preview.json';
 
-                                /*
-                                // Get image info:
-                                // UPDATE - note this was done BEFORE innerHTML but prefomed very badly
-                                // when there are a lot of files.The code used id below. It should be
-                                // possible to just do the innerHMTML then go back through and update
-                                // width/heigh, but there doesn't seem much point as those attributes
-                                // are not acually doing anything for the display (that I can see)
-                                var json_url = window.location.origin + file + '.preview.json';
+                                    fetch(json_url)
+                                        .then((response) => {
+                                            if (!response.ok) {
+                                                throw new Error(`HTTP error: ${response.status}`);
+                                            }
+                                            return response.text();
+                                        })
+                                        .then((text) => {
+                                            img_info = JSON.parse(text);
+                                            width = img_info.image_width;
+                                            height = img_info.image_height;
 
-                                fetch(json_url)
-                                    .then((response) => {
-                                        if (!response.ok) {
-                                            throw new Error(`HTTP error: ${response.status}`);
-                                        }
-                                        return response.text();
-                                    })
-                                    .then((text) => {
-                                        img_info = JSON.parse(text);
-                                        width = img_info.image_width;
-                                        height = img_info.image_height;
-
-                                        let img = `<img
-                                            class="image-cropped"
-                                            src="${file}"
-                                            alt="${el.title}"
-                                            loading="lazy"
-                                            width="${width}"
-                                            height="${height}"
-                                        >`;
-                                        container.innerHTML = img;
-                                    })
-                                    .catch((error) => {
-                                        console.warn(error);
-                                    });
-                                */
+                                            let img = `<img
+                                                class="image-cropped"
+                                                src="${file}"
+                                                alt="${el.title}"
+                                                loading="lazy"
+                                                width="${width}"
+                                                height="${height}"
+                                            >`;
+                                            container.innerHTML = img;
+                                        })
+                                        .catch((error) => {
+                                            console.warn(error);
+                                        });
+                                    */
+                                }
                             }
                         }
                     }
